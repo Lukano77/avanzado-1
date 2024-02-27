9e959fac6eba97a9637eb9d9e211150ab26bb52d
Lukano77 <luciano_cuesta@hotmail.com>
2024-02-27 18:52:59 -0300

    Add login to commands,add Post to create a Product

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -29,3 +29,21 @@ Cypress.Commands.add('getByDataCy', (selector) => {
     return cy.get(`[data-cy=${selector}]`)
 });
 
+
+Cypress.Commands.add('login', (usuario,password)=>{
+    cy.request({
+        method: "POST",
+        url: `${Cypress.env().baseUrlApi}/login`,
+        body: {
+            username: usuario,
+            password: password
+        },
+    }).then(respuesta => {
+        window.localStorage.setItem('token', respuesta.body.token);
+        window.localStorage.setItem('user', respuesta.body.user.username);
+        window.localStorage.setItem('userId', respuesta.body.user_id);
+        Cypress.env().token = respuesta.body.token;
+    })
+    cy.visit('');
+
+})
