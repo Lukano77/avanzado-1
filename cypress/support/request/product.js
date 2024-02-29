5b36bbef1abbfb3bcfd275e543b0f9b14e83388b
Lukano77 <luciano_cuesta@hotmail.com>
2024-02-29 19:57:05 -0300

    Fix to Add new Product and Added an Edit Product by API

--- cypress/support/request/product.js
+++ cypress/support/request/product.js
@@ -1,4 +1,4 @@
-Cypress.Commands.add('eliminarProducto',(id)=>{
+Cypress.Commands.add('eliminarProducto', (id) => {
     cy.request({
         method: "GET",
         url: `${Cypress.env().baseUrlApi}/products?id=${id}`,
@@ -16,10 +16,42 @@ Cypress.Commands.add('eliminarProducto',(id)=>{
     })
 });
 
-Cypress.Commands.add('crearProducto',(body)=>{
+Cypress.Commands.add('crearProducto', (id, productName, productPrice, ProductImageUrl) => {
     cy.request({
-        method:"POST",
-        url:`${Cypress.env().baseUrlApi}/create-product`,
-        body:body
+        method: "POST",
+        url: `${Cypress.env().baseUrlApi}/create-product`,
+        headers: {
+            Authorization: `Bearer ${Cypress.env().token}`
+        },
+        body: {
+
+            "name": productName,
+            "price": productPrice,
+            "img": ProductImageUrl,
+            "id": id
+        }
+    })
+})
+
+Cypress.Commands.add('editarProducto', (id) => {
+    cy.request({
+        method: "GET",
+        url: `${Cypress.env().baseUrlApi}/products?id=${id}`,
+        headers: {
+            Authorization: `Bearer ${Cypress.env().token}`
+        }
+    }).its('body.products.docs').each((product) => {
+        cy.request({
+            method: "PUT",
+            url: `${Cypress.env().baseUrlApi}/product/${product._id}`,
+            headers: {
+                Authorization: `Bearer ${Cypress.env().token}`
+            },
+            body: {
+                "name": "Carteras",
+                "price": "100.35",
+                "img": "https://margies.shop/cdn/shop/files/Ron-Negro-2_800x.jpg?v=1696445384"
+            }
+        })
     })
 })
